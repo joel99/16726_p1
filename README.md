4f0d9dd49269f4f88b6e0862a47222f0a799f729
Joel Ye <joelye9@gmail.com>
2023-02-14 11:47:12 -0500

    post

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# 16726_p1
+# 16-726 P1: Colorizing via Alignment
+
+For this project we were given single-channel color slides taken in the early 19th century; the goal was to produce a full color composite, by aligning the individual pieces. Specifically, we would align red and green plates to the blue plate.
+
+Minimal distortion was assumed, particularly in the centers of the image, so alignment is performed by sweeping for an optimal match in this region. This match is computed with your metric of choice, but we were prompted with NCC and SSD as simple starters. These were indeed sufficient in most cases.
+
+The sweeping was done by searching a small grid; for larger images this can be sped up using coarse-to-fine multiresolution methods. Specifically, I resized all images such that the largest dimension was under 100 pixels, and assume that the offset is less than 4 pixels (i.e. 4%) in this resolution. After finding the optimal match at this small resolution I would resize back up by factors of 2; adjusting the search range accordingly.
+
+Some issues: notably, SSD is pretty brittle and actually fails if the search range is too large; a small translation assumption is helpful. NCC is more robust; I'm sure other methods would be even better. I also suspect sweeping for more than I'm upsampling by at each step is unnecessary.
+
+## Results
+### Example set
+
+Initial results shown with the simplest SSD metric.
+
+[Cathedral](./out/numpy_base/ssd/cathedral_g_(5%2C%202)_r_(12%2C%203).jpg)
+
+[Self Portrait](./out/numpy_base/ssd/self_portrait_g_(78%2C%2029)_r_(175%2C%2037).jpg)
+
+[Icon](./out/numpy_base/ssd/icon_g_(41%2C%2017)_r_(90%2C%2023).jpg)
+
+
+[Lady](./out/numpy_base/ssd/lady_g_(55%2C%208)_r_(115%2C%2012).jpg)
+
+[Three generations](./out/numpy_base/ssd/three_generations_g_(52%2C%2014)_r_(111%2C%2012).jpg)
+
+[Turkmen](./out/numpy_base/ssd/turkmen_g_(55%2C%2021)_r_(116%2C%2029).jpg)
+
+[Village](./out/numpy_base/ssd/village_g_(64%2C%2012)_r_(137%2C%2022).jpg)
+
+[Train](./out/numpy_base/ssd/train_g_(43%2C%206)_r_(86%2C%2032).jpg)
+
+[Harvesters](./out/numpy_base/ssd/harvesters_g_(59%2C%2017)_r_(123%2C%2014).jpg)
+There's an artifact on the left of this image but this seems unlikely to be a misalignment since the center of the image is fine. Rather, perhaps the person moved, or there's local warping.
+
+
+[Emir](./out/numpy_base/ssd/emir.jpg)
+This is a drastic misalignment.
+
+| Image | Green Offset (X, Y) | Red Offset (X, Y) |
+| ---: | --- | --- |
+| Cathedral | 5, 2 | 12, 3 |
+| Self-portrait | 78, 29 | 175, 37 |
+| Icon | 41, 17 | 90, 23|
+| Lady | 55, 8 | 115, 12 |
+| Three-gen | 52, 14,| 111, 12 |
+| Harvesters | 59, 17 | 123, 14 |
+| Turkmen | 55, 21 | 116, 29 |
+| Village | 64, 12 | 137, 22 |
+| Train | 43, 6 | 86, 32 |
+| Emir | 48, 24 | 136, -280 |
+
+Then the NCC metric.
+
+TODO links
+
+| Image | Green Offset (X, Y) | Red Offset (X, Y) |
+| ---: | --- | --- |
+| Cathedral | 5, 2 | 12, 3 |
+| Self-portrait | 78, 29 | 175, 37 |
+| Icon | 41, 17 | 90 23 |
+| Lady | 54, 8 | 116, 12 |
+| Three-gen | 52, 14 | 111, 12 |
+| Harvesters | 59, 17 | 123, 14 |
+| Turkmen | 55, 21 | 116, 28 |
+| Village | 64, 12 | 137, 22 |
+| Train | 42, 6 | 86, 32 |
+| Emir | 48, 24 | -330, 17 |
+
+There are only 1 pixel disagreements except for Emir, which is a failure anyway.
+
+### Prokudin-Gorskii extra examples
+I noted some SSD fragility in development; this is reiterated her.SSD fails on at least one plate here, but NCC works without tuning hyperparameters. It's hard to pinpoint an exact qualitative difference between these and the given demos (to explain the total failure here), so I would conclude that SSD is just too brittle to use in general.
+
+TODO links
+
+| Image | SSD Green Offset | SSD Red Offset | NCC Green Offset | NCC Red Offset |
+| ---: | --- | --- | --- | --- |
+| River | 25, -5 | 153, -213 | 25, -5 | 102, -11 |
+| Railroad | 101,-428 | 102, 2 | 26, 1 | 102, 2|
+| Grass | -484, -474 | 52, 21 | 22, 15 | 52, 21 |
+
+
+
+## Failure modes
+TODO
+
+
+
+
+## Bells and Whistles - Pytorch
+This produces no difference in computed offsets and so I won't repost images -- (posting two comptued offsets with NCC metric for brevity). I swapped all functions except the rescaling; which I still used `skt` for.
+
+| Image | Green Offset (X, Y) | Red Offset (X, Y) |
+| ---: | --- | --- |
+| Self-portrait | 78, 29 | 175, 37 |
+| Emir | 48, 24 | -330, 17 |
--- main_hw1.py
+++ main_hw1.py
@@ -12,19 +12,23 @@ import skimage.transform as skt
 import torch
 import argparse
 
+use_pytorch = False
+use_pytorch = True
+lib_func = torch if use_pytorch else np
 BASE_SWEEP = 30 # used for cathedral i.e. when sweep isn't specified for single-scale
+BASE_SWEEP_RANGE = torch.tensor([-BASE_SWEEP, BASE_SWEEP]) if use_pytorch else np.array([-BASE_SWEEP, BASE_SWEEP])
 def score_similarity(im1: torch.Tensor, im2: torch.Tensor, mode='ssd') -> float:
     # Return higher is better
     # SSD for starters
     if mode == 'ssd':
-        return -torch.sum((im1 - im2) ** 2)
+        return -lib_func.sum((im1 - im2) ** 2)
     elif mode == 'ncc':
-        return torch.sum(im1 * im2) / (torch.sqrt(torch.sum(im1 ** 2)) * torch.sqrt(torch.sum(im2 ** 2)))
+        return lib_func.sum(im1 * im2) / (lib_func.sqrt(lib_func.sum(im1 ** 2)) * lib_func.sqrt(lib_func.sum(im2 ** 2)))
     else:
         raise NotImplementedError
 
 
-def main(input_path, metric, crop_pct, sweep):
+def main(input_path, tag, metric, crop_pct, sweep):
     # name of the input file
     imname = Path(input_path)
 
@@ -36,7 +40,6 @@ def main(input_path, metric, crop_pct, sweep):
 
     # compute the height of each part (just 1/3 of total)
     height = np.floor(im.shape[0] / 3.0).astype(int)
-
     # separate color channels
     b = im[:height]
     g = im[height: 2*height]
@@ -45,31 +48,36 @@ def main(input_path, metric, crop_pct, sweep):
     def align(
         src: np.ndarray,
         target: np.ndarray,
-        sweep_x=[-BASE_SWEEP, BASE_SWEEP],
-        sweep_y=[-BASE_SWEEP, BASE_SWEEP],
+        sweep_x=BASE_SWEEP_RANGE,
+        sweep_y=BASE_SWEEP_RANGE,
         crop_pct=crop_pct
     ):
         # print(src.shape, target.shape, sweep_x, sweep_y, crop_pct)
-        src = torch.tensor(src)
-        target = torch.tensor(target)
-        scores = torch.zeros((sweep_x[1] - sweep_x[0], sweep_y[1] - sweep_y[0]))
+        if use_pytorch:
+            src = torch.tensor(src)
+            target = torch.tensor(target)
+        scores = lib_func.zeros((sweep_x[1] - sweep_x[0], sweep_y[1] - sweep_y[0]))
         for i, offset_x in enumerate(range(sweep_x[0], sweep_x[1])):
             for j, offset_y in enumerate(range(sweep_y[0], sweep_y[1])):
-                shift_src = torch.roll(src, (offset_x, offset_y), dims=(0, 1))
+                shift_src = lib_func.roll(src, (offset_x, offset_y), (0, 1))
                 # crop both shift_src and target for comparison
                 # if shift is positive, then the start is invalid
                 # if shift is negative, then the end is invalid
                 def get_valid_crop(src: torch.Tensor, offset, axis=0):
-                    indices = torch.arange(src.size(axis))
-                    valid_mask = torch.ones(indices.size(), dtype=torch.bool)
+                    indices = lib_func.arange(src.shape[axis])
+                    valid_mask = lib_func.ones(indices.shape, dtype=bool)
                     valid_mask[:max(0, offset)] = False
                     if offset < 0:
                         valid_mask[offset:] = False
                     indices = indices[valid_mask]
-                    return torch.index_select(src, axis, indices)
+                    if use_pytorch:
+                        return torch.index_select(src, axis, indices)
+                    else:
+                        return np.take(src, indices, axis=axis)
                 def center_crop(src: torch.Tensor, pct):
-                    center_x = torch.div(src.shape[0], 2)
-                    center_y = torch.div(src.shape[1], 2)
+                    divide_func = torch.div if use_pytorch else np.divide
+                    center_x = divide_func(src.shape[0], 2)
+                    center_y = divide_func(src.shape[1], 2)
                     span_x = (src.shape[0] * pct / 2)
                     span_y = (src.shape[1] * pct / 2)
                     return src[int(center_x - span_x):int(center_x + span_x), int(center_y - span_y):int(center_y + span_y)]
@@ -82,16 +90,21 @@ def main(input_path, metric, crop_pct, sweep):
                 target_valid = center_crop(target_valid, crop_pct)
                 score = score_similarity(shift_src_valid, target_valid, mode=metric)
                 scores[i, j] = score
-        best_index = torch.argmax(scores)
-        roll_x = range(sweep_x[0], sweep_x[1])[torch.div(best_index, scores.shape[0], rounding_mode='trunc')]
+        best_index = lib_func.argmax(scores)
+        x_index = torch.div(best_index, scores.shape[0], rounding_mode='trunc') if use_pytorch else best_index // scores.shape[0]
+        roll_x = range(sweep_x[0], sweep_x[1])[x_index]
         roll_y = range(sweep_y[0], sweep_y[1])[best_index % scores.shape[0]]
-        shift_src = torch.roll(src, (roll_x, roll_y), dims=(0, 1))
+        shift_src = lib_func.roll(src, (roll_x, roll_y), (0, 1))
         # print(f'{roll_x} from {sweep_x}, {roll_y} from {sweep_y}')
         # print(roll_x, roll_y)
         return shift_src, (roll_x, roll_y)
 
     def align_multiscale(src: np.ndarray, target: np.ndarray, scale_res_min=100, iter_factor=2, sweep_range=[-sweep, sweep]):
         assert src.shape == target.shape # TODO - support different shapes
+        if use_pytorch:
+            sweep_range = torch.tensor(sweep_range)
+        else:
+            sweep_range = np.array(sweep_range)
         scale_factor = 1
         scale_shape = max(src.shape)
         while scale_shape > scale_res_min:
@@ -105,34 +118,34 @@ def main(input_path, metric, crop_pct, sweep):
             src_scale = skt.rescale(src, 1 / scale_factor, anti_aliasing=True)
             target_scale = skt.rescale(target, 1 / scale_factor, anti_aliasing=True)
             # print(f'scale: {scale_factor} roll center: {roll_x}, {roll_y}')
-            _, (roll_x, roll_y) = align(src_scale, target_scale, sweep_x=np.array(sweep_range) + roll_x, sweep_y=np.array(sweep_range) + roll_y)
+            _, (roll_x, roll_y) = align(src_scale, target_scale, sweep_x=sweep_range + roll_x, sweep_y=sweep_range + roll_y)
             # print(f'scale: {scale_factor} roll after: {roll_x}, {roll_y}')
             scale_factor = int(scale_factor / iter_factor)
             # while the scaling will have rounding errors, the extra sweeping margin should be more than enough to make up for it.
-        shift_src = torch.roll(torch.tensor(src), (roll_x, roll_y), dims=(0, 1))
-        # print('final ', roll_x, roll_y)
+        if use_pytorch:
+            src = torch.tensor(src)
+        shift_src = lib_func.roll(src, (roll_x, roll_y), (0, 1))
         return shift_src, (roll_x, roll_y)
 
-    # if max(b.shape) > 400:
-    # print('green')
-    ag, _ = align_multiscale(g, b, scale_res_min=100, iter_factor=2, sweep_range=[-sweep, sweep])
+    ag, rolls_g = align_multiscale(g, b, scale_res_min=100, iter_factor=2, sweep_range=[-sweep, sweep])
+    ar, rolls_r = align_multiscale(r, b, scale_res_min=100, iter_factor=2, sweep_range=[-sweep, sweep])
     # ag, _ = align(g, b, crop_pct=crop_pct)
-    # print('red')
-    # else:
-    ar, _ = align_multiscale(r, b, scale_res_min=100, iter_factor=2, sweep_range=[-sweep, sweep])
     # ar, _ = align(r, b, crop_pct=crop_pct)
-    im_out = torch.stack([ar, ag, torch.tensor(b)], -1)
-    im_out = (im_out * 255).numpy().astype(np.uint8)
-    # im_out = np.dstack([ar, ag, b])
-
+    if use_pytorch:
+        im_out = torch.stack([ar, ag, torch.tensor(b)], -1)
+        im_out = (im_out).numpy()
+    else:
+        im_out = np.dstack([ar, ag, b])
+    im_out = (im_out * 255).astype(np.uint8)
     # save the image
-    os.makedirs('out', exist_ok=True)
-    fname = f'out/{imname.stem}.jpg'
+    out_root = Path('out') / tag / metric
+    os.makedirs(out_root, exist_ok=True)
+    fname = out_root / f'{imname.stem}_g_{rolls_g}_r_{rolls_r}.jpg'
     skio.imsave(fname, im_out)
 
     # display the image
-    skio.imshow(im_out)
-    skio.show()
+    # skio.imshow(im_out)
+    # skio.show()
 
 # Notes
 # SSD is trash, cathedral doesn't work on it even just with a slightly larger than 4x4 search range; too brittle
@@ -141,6 +154,7 @@ if __name__ == '__main__':
     # parse for input file
     parser = argparse.ArgumentParser()
     parser.add_argument('--input-path', '-i', type=str, default='data/cathedral.jpg')
+    parser.add_argument('--tag', '-t', type=str, default='numpy_base')
     parser.add_argument('--metric', '-m', type=str, default='ssd') # ssd, ncc
     parser.add_argument('--crop-pct', '-c', type=float, default=0.5, help='pct of image to compare')
     parser.add_argument('--sweep', '-s', type=int, default=4, help='brute force search range')
